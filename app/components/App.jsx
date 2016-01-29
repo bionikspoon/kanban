c830af0589f2447b635a7bbc2fa6b68b839965ea
Manu Phatak <bionikspoon@gmail.com>
2016-01-28 22:29:11 -0600

    move notes to constructor

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,8 +1,52 @@
+import uuid from 'node-uuid';
 import React from 'react';
-import Note from './Note.jsx';
 
+//noinspection JSUnusedLocalSymbols,JSUnresolvedVariable
 export default class App extends React.Component {
-  render() {
-    return <Note />;
+  constructor(props) {
+    super(props);
+    //noinspection JSUnresolvedFunction
+    this.state = {
+      notes: [
+        {
+          id:   uuid.v4(),
+          task: 'Learn Webpack'
+        },
+        {
+          id:   uuid.v4(),
+          task: 'Learn React'
+        },
+        {
+          id:   uuid.v4(),
+          task: 'Take out the trash'
+        }
+      ]
+    };
   }
+
+  render() {
+    const notes = this.state.notes.map(note =><li key={note.id}>{note.task}</li>);
+
+    return (
+      <div>
+        <button onClick={this.addNote}>+</button>
+        <ul>
+          {notes}
+        </ul>
+      </div>
+    );
+  }
+
+  addNote = () => {
+    //noinspection JSUnresolvedFunction
+    this.setState({
+      notes: this.state.notes.concat([
+        {
+          id:   uuid.v4(),
+          task: 'New task'
+        }
+      ])
+    });
+  };
+
 }
