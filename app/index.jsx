263cef5204dea96db36824e8c9d41d34857e8414
Manu Phatak <bionikspoon@gmail.com>
2016-01-29 18:34:10 -0600

    persist state in localstorage

--- app/index.jsx
+++ app/index.jsx
@@ -2,5 +2,9 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import App from './components/App.jsx';
 import './main.css';
+import alt from './libs/alt';
+import storage from './libs/storage';
+import persist from './libs/persist';
 
+persist(alt, storage, 'app');
 ReactDom.render(<App />, document.getElementById('app'));
