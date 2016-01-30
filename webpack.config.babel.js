338292b565036ed0e3dfbb516db3e747b2d26f87
Manu Phatak <bionikspoon@gmail.com>
2016-01-30 02:40:28 -0600

    setup webpack build process

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,17 +2,21 @@ const path = require('path');
 const merge = require('webpack-merge');
 const webpack = require('webpack');
 const stylelint = require('stylelint');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const Clean = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const pkg = require('./package.json');
 
 const TARGET = process.env.npm_lifecycle_event;
-process.env.BABEL_ENV = TARGET;
-
 const PATHS = {
-  app:   path.join(__dirname, 'app'),
+  app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
+process.env.BABEL_ENV = TARGET;
+
 const common = {
-  entry:   PATHS.app,
+  entry: PATHS.app,
   resolve: {
     extensions: [
       '',
@@ -20,19 +24,19 @@ const common = {
       '.jsx'
     ]
   },
-  output:  {
-    path:     PATHS.build,
-    filename: 'bundle.js'
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
   },
-  module:  {
+  module: {
     preLoaders: [
       {
-        test:    /\.css$/,
+        test: /\.css$/,
         loaders: ['postcss'],
         include: PATHS.app
       },
       {
-        test:    /\.jsx?$/,
+        test: /\.jsx?$/,
         loaders: [
           'eslint',
           'jscs'
@@ -40,22 +44,23 @@ const common = {
         include: PATHS.app
       }
     ],
-    loaders:    [
+    loaders: [
       {
-        test:    /\.css$/,
-        loaders: [
-          'style',
-          'css'
-        ],
-        include: PATHS.app
-      },
-      {
-        test:    /\.jsx?$/,
+        test: /\.jsx?$/,
         loaders: ['babel?cacheDirectory'],
         include: PATHS.app
       }
     ]
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'node_modules/html-webpack-template/index.html',
+      title: 'Kanban app',
+      appMountId: 'app',
+      inject: false
+    })
+
+  ],
   postcss: function() {
     return [
       stylelint({
@@ -70,22 +75,74 @@ const common = {
 if(TARGET === 'start' || !TARGET) {
   module.exports = merge(common, {
     devServer: {
-      contentBase:        PATHS.build,
       historyApiFallback: true,
-      hot:                true,
-      inline:             true,
-      progress:           true,
-      stats:              'errors-only',
-      host:               process.env.HOST,
-      port:               process.env.PORT
+      hot: true,
+      inline: true,
+      progress: true,
+      stats: 'errors-only',
+      host: process.env.HOST,
+      port: process.env.PORT
     },
-    plugins:   [
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loaders: [
+            'style',
+            'css'
+          ],
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
       new webpack.HotModuleReplacementPlugin()
     ],
-    devtool:   'eval-source-map'
+    devtool: 'eval-source-map'
   });
 }
 
-if(TARGET === 'build') {
-  module.exports = merge(common, {});
+if(TARGET === 'build' || TARGET === 'stats') {
+  module.exports = merge(common, {
+    entry: {
+      app: PATHS.app,
+      vendor: Object.keys(pkg.dependencies).filter(function(v) {
+        return v !== 'alt-utils';
+      })
+    },
+    output: {
+      path: PATHS.build,
+      filename: '[name].[chunkhash].js',
+      chunkFilename: '[chunkhash].js'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract('style', 'css'),
+          include: PATHS.app
+        }
+      ]
+    },
+    plugins: [
+      new Clean([PATHS.build], {
+        verbose: false
+      }),
+      new ExtractTextPlugin('styles.[chunkhash].css'),
+      new webpack.optimize.CommonsChunkPlugin({
+        names: [
+          'vendor',
+          'manifest'
+        ]
+      }),
+      new webpack.DefinePlugin({
+        'process.env.NODE_ENV': JSON.stringify('production')
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })
+    ]
+  });
 }
