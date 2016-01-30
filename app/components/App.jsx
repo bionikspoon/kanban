ce29cc79c254636d4184336597fe2770debb2612
Manu Phatak <bionikspoon@gmail.com>
2016-01-30 00:50:27 -0600

    scaffold react dnd

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -5,7 +5,11 @@ import LaneActions from '../actions/LaneActions';
 import LaneStore from '../stores/LaneStore';
 import Lanes from './Lanes.jsx';
 
-class App extends React.Component {
+import {DragDropContext} from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
+
+@DragDropContext(HTML5Backend)
+export default class App extends React.Component {
   render() {
     const stores = {LaneStore};
     const inject = {
@@ -31,4 +35,3 @@ class App extends React.Component {
   }
 
 }
-export default App;
--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,76 +1,33 @@
 import React from 'react';
 
+import {DragSource, DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
+
+const noteSource = {
+  beginDrag(props) {
+    console.log('begin dragging note', props);
+    return {};
+  }
+};
+
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+
+    console.log('dragging note', sourceProps, targetProps);
+  }
+};
+
+@DragSource(ItemTypes.NOTE, noteSource, (connect) => ({
+  connectDragSource: connect.dragSource()
+}))
+
+@DropTarget(ItemTypes.NOTE, noteTarget, (connect)=>({
+  connectDropTarget: connect.dropTarget()
+}))
 export default class Note extends React.Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      editing: false
-    };
-  }
-
   render() {
-    if(this.state.editing) {
-      return this.renderEdit();
-    }
-
-    return this.renderNote();
+    const {connectDragSource, connectDropTarget, id, onMove, ...props} = this.props;
+    return connectDragSource(connectDropTarget(<li {...props}>{props.children}</li>));
   }
-
-  renderEdit = () => {
-    const ref = (e) => e ? e.selectionStart = this.props.task.length : null;
-
-    return <input
-      type="text"
-      ref={ref}
-      autoFocus={true}
-      defaultValue={this.props.task}
-      onBlur={this.finishEdit}
-      onKeyPress={this.checkEnter}
-    />;
-  };
-
-  renderDelete = () => {
-    return <button
-      className="delete-note"
-      onClick={this.props.onDelete}>x</button>;
-  };
-
-  renderNote = () => {
-    const onDelete = this.props.onDelete;
-
-    return (
-      <div onClick={this.edit}>
-        <span className="task">{this.props.task}</span>
-
-        {onDelete ? this.renderDelete() : null}
-      </div>
-    );
-  };
-
-  edit = () => {
-    this.setState({
-      editing: true
-    });
-  };
-
-  checkEnter = (e) => {
-    if(e.key === 'Enter') {
-      this.finishEdit(e);
-    }
-  };
-
-  finishEdit = e => {
-    const value = e.target.value;
-
-    if(this.props.onEdit && value.trim()) {
-      this.props.onEdit(value);
-
-      this.setState({
-        editing: false
-      });
-    }
-  };
-
 }
